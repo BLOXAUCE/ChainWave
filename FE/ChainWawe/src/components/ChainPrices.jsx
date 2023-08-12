577fc90182c3a1823fdd4c1e75d408a2cf674c31
Rabeles11 <9david@seznam.cz>
2023-08-13 01:33:00 +0200

    Metamask and chainprices

--- FE/ChainWawe/src/components/ChainPrices.jsx
+++ FE/ChainWawe/src/components/ChainPrices.jsx
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 import mumbai from "../assets/mumbai.png";
 import theme from "../theme/theme";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 
 const ChainWrapper = styled.div`
   display: flex;
+  flex-wrap: wrap;
   gap: 5rem;
   flex-direction: row;
   align-items: center;
@@ -29,28 +32,71 @@ const ChainImage = styled.img`
   }
 `;
 
+const ChainSwitchButton = styled.button`
+  background: transparent;
+  font-size: clamp(1.1rem, 0.8vw, 1.1rem);
+  color: white;
+  cursor: pointer;
+  border: 1px solid white;
+`;
+
 function ChainPrices() {
+  const [gasPriceFuji, setGasPriceFuji] = useState(null);
+  const [gasPriceBSC, setGasPriceBSC] = useState(null);
+  const [gasPriceFantom, setGasPriceFantom] = useState(null);
+  const [gasPriceSepolia, setGasPriceSepolia] = useState(null);
+
+  const RPC_URL_FUJI = "https://ava-testnet.public.blastapi.io/ext/bc/C/rpc";
+  const RPC_URL_BSC = "https://bsc-testnet.publicnode.com";
+  const RPC_URL_FANTOM = "https://rpc.ankr.com/fantom_testnet";
+  const RPC_URL_SEPOLIA = "https://sepolia.gateway.tenderly.co";
+
+  useEffect(() => {
+    const fetchGasPrice = async () => {
+      let provider = new ethers.providers.JsonRpcProvider(RPC_URL_FUJI);
+      let price = await provider.getGasPrice();
+      setGasPriceFuji(ethers.utils.formatUnits(price, "gwei"));
+
+      provider = new ethers.providers.JsonRpcProvider(RPC_URL_BSC);
+      price = await provider.getGasPrice();
+      setGasPriceBSC(ethers.utils.formatUnits(price, "gwei"));
+
+      provider = new ethers.providers.JsonRpcProvider(RPC_URL_FANTOM);
+      price = await provider.getGasPrice();
+      setGasPriceFantom(ethers.utils.formatUnits(price, "gwei"));
+
+      provider = new ethers.providers.JsonRpcProvider(RPC_URL_SEPOLIA);
+      price = await provider.getGasPrice();
+      setGasPriceSepolia(ethers.utils.formatUnits(price, "gwei"));
+    };
+
+    fetchGasPrice();
+  }, []);
   return (
     <ChainWrapper>
       <ChainItem>
-        Mumbai
+        Fuji
         <ChainImage src={mumbai} />
-        15 gwei
+        {parseFloat(gasPriceFuji).toFixed(2)} gwei
+        <ChainSwitchButton>Switch</ChainSwitchButton>
       </ChainItem>
       <ChainItem>
-        Mumbai
+        BSC
         <ChainImage src={mumbai} />
-        15 gwei
+        {parseFloat(gasPriceBSC).toFixed(2)} gwei
+        <ChainSwitchButton>Switch</ChainSwitchButton>
       </ChainItem>
       <ChainItem>
-        Mumbai
+        Fantom
         <ChainImage src={mumbai} />
-        15 gwei
+        {parseFloat(gasPriceFantom).toFixed(2)} gwei
+        <ChainSwitchButton>Switch</ChainSwitchButton>
       </ChainItem>
       <ChainItem>
-        Mumbai
+        Sepolia
         <ChainImage src={mumbai} />
-        15 gwei
+        {parseFloat(gasPriceSepolia).toFixed(2)} gwei
+        <ChainSwitchButton>Switch</ChainSwitchButton>
       </ChainItem>
     </ChainWrapper>
   );
--- FE/ChainWawe/src/components/Header.jsx
+++ FE/ChainWawe/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import theme from "../theme/theme";
 import { useNavigate } from "react-router";
+import { useState, useEffect } from "react";
 
 const HeaderWrapper = styled.div`
   display: flex;
@@ -21,7 +22,7 @@ const HeaderWrapper = styled.div`
 const HeaderItem = styled.div`
   position: relative;
   display: flex;
-  font-size: clamp(1.7rem, 1vw, 2rem);
+  font-size: clamp(1.5rem, 1vw, 1rem);
   z-index: 2;
   padding: 0.5rem 1rem;
   font-weight: 400;
@@ -38,6 +39,48 @@ const HeaderItemLogin = styled(HeaderItem)`
 
 function Header() {
   const navigate = useNavigate();
+  const [isConnected, setIsConnected] = useState(false);
+  const [networkId, setNetworkId] = useState(null); // State to hold the network ID
+  const [selectedAddress, setSelectedAddress] = useState(null);
+
+  const connectToMetaMask = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.enable();
+        setIsConnected(true);
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+        setSelectedAddress(accounts[0]);
+        const networkId = await window.ethereum.request({
+          method: "eth_chainId",
+        });
+        setNetworkId(networkId);
+      } catch (error) {
+        console.error("Error connecting to MetaMask:", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    // Listen for changes in the Ethereum address
+    window.ethereum.on("accountsChanged", (accounts) => {
+      setSelectedAddress(accounts[0]);
+      setIsConnected(!!accounts.length);
+    });
+
+    // Listen for changes in the Ethereum network
+    window.ethereum.on("chainChanged", (networkId) => {
+      setNetworkId(networkId);
+    });
+
+    return () => {
+      // Cleanup the event listeners when the component unmounts
+      window.ethereum.removeAllListeners("accountsChanged");
+      window.ethereum.removeAllListeners("chainChanged");
+    };
+  }, []);
+
   return (
     <>
       <HeaderWrapper>
@@ -45,7 +88,9 @@ function Header() {
         <HeaderItem onClick={() => navigate("/newProject")}>
           New project
         </HeaderItem>
-        <HeaderItemLogin>Login</HeaderItemLogin>
+        <HeaderItemLogin onClick={connectToMetaMask}>
+          {isConnected ? `Connected ` : "Login"}
+        </HeaderItemLogin>
       </HeaderWrapper>
     </>
   );
