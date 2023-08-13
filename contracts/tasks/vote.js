515b320d9302dd0b52f6ade28df4fe4812a5bebb
Rabeles11 <9david@seznam.cz>
2023-08-13 13:18:05 +0200

    theGraph

--- contracts/tasks/vote.js
+++ contracts/tasks/vote.js
@@ -7,14 +7,14 @@ module.exports = async function (taskArgs, hre) {
 
     console.log("Sending vote to contract: " + votingSender.address)
 
-    const adapterParams = ethers.utils.solidityPack(["uint16", "uint256"], [1, 900000]) // default adapterParams example
+    const adapterParams = ethers.utils.solidityPack(["uint16", "uint256"], [1, 2000000]) // default adapterParams example
     const gasPriceWei = await ethers.provider.getGasPrice()
     const gasPriceGwei = ethers.utils.formatUnits(gasPriceWei, "gwei")
     const endpoint = await ethers.getContractAt("ILayerZeroEndpoint", ENDPOINTS[hre.network.name])
     const fees = await endpoint.estimateFees(remoteChainId, votingSender.address, "0x", false, adapterParams)
     console.log(`fees[0] (wei): ${fees[0]} / (eth): ${ethers.utils.formatEther(fees[0])}`)
 
-    const tx = await (await votingSender.sendVote(1, 1, true, [], { value: fees[0] })).wait()
+    const tx = await (await votingSender.sendVote(1, 1, true, [], { value: fees[0] + 222 })).wait()
     console.log(`✅ Message Sent`)
     console.log(`tx: ${tx.transactionHash}`)
 }
