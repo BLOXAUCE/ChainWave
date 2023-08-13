d345849bf77ad32bca6ab44053e4b098d3a23a76
Jan Smrza <smrza.jan@gmail.com>
2023-08-13 11:06:57 +0200

    new testnet deployment

--- contracts/tasks/createProposal.js
+++ contracts/tasks/createProposal.js
@@ -3,7 +3,7 @@ module.exports = async function (taskArgs, hre) {
     console.log("Creating proposal in Executor contract: " + votingExecutor.address)
 
     let tx = await (
-        await votingExecutor.createProposal(0, 1692732803, 8, "0x0000000000000000000000000000000000000000000000000000000000000000")
+        await votingExecutor.createProposal(0, 1692732803, 5, "0x0000000000000000000000000000000000000000000000000000000000000000")
     ).wait()
     console.log(`✅ Proposal crated`)
     console.log(`tx: ${tx.transactionHash}`)
